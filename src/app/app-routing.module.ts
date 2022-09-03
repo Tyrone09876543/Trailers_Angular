9f76a8cf8727ded67c10641c8ccdc688ca0801a3
Tyrone09876543 <rubik09876543@gmail.com>
2022-09-03 19:01:43 -0400

    login form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AccountComponent } from './account/account.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {path:'', component:HomeComponent},
-  {path: '**',redirectTo: ''
-  }
+  {path:'account', component:AccountComponent},
+  {path:'login', component:LoginComponent},
+  {path: '**',redirectTo: ''},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { TrailersComponent } from './trailers/trailers.component';
+import { DetailsComponent } from './trailers/details/details.component';
+import { AccountComponent } from './account/account.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    TrailersComponent,
+    DetailsComponent,
+    AccountComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
